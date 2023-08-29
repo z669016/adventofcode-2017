99d611ee20ef634d06a4bccc3a88d9f0965668c6
Rene van Putten <z669016@gmail.com>
2023-08-29 18:57:59 +0200

    Added Timer, and introduced var, reduced scope where possible, minor code enhancements

--- src/main/java/com/putoet/day13/Day13.java
+++ src/main/java/com/putoet/day13/Day13.java
@@ -3,8 +3,6 @@ package com.putoet.day13;
 import com.putoet.resources.ResourceLines;
 import com.putoet.utils.Timer;
 
-import java.util.List;
-
 public class Day13 {
     public static void main(String[] args) {
         final Firewall firewall = Firewall.of(ResourceLines.list("/day13.txt"));
