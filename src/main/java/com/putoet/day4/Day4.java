b1f6c584753409821d7f83a550fdb67c0dfd3564
Rene van Putten <z669016@gmail.com>
2023-08-29 10:29:01 +0200

    Added Timer, and introduced var, reduced scope where possible, minor code enhancements

--- src/main/java/com/putoet/day4/Day4.java
+++ src/main/java/com/putoet/day4/Day4.java
@@ -1,17 +1,20 @@
 package com.putoet.day4;
 
 import com.putoet.resources.ResourceLines;
-
-import java.util.List;
+import com.putoet.utils.Timer;
 
 public class Day4 {
     public static void main(String[] args) {
-        final List<String> phrases = ResourceLines.list("/day4.txt");
+        final var phrases = ResourceLines.list("/day4.txt");
 
-        long valid = phrases.stream().filter(PassPhrase::isValidNoDoubles).count();
-        System.out.println("Number of valid pass phrases (no doubles) is " + valid);
+        Timer.run(() -> {
+            final var valid = phrases.stream().filter(PassPhrase::isValidNoDoubles).count();
+            System.out.println("Number of valid pass phrases (no doubles) is " + valid);
+        });
 
-        valid = phrases.stream().filter(PassPhrase::isValidNoAnagrams).count();
-        System.out.println("Number of valid pass phrases (no anagrams) is " + valid);
+        Timer.run(() -> {
+            final var valid = phrases.stream().filter(PassPhrase::isValidNoAnagrams).count();
+            System.out.println("Number of valid pass phrases (no anagrams) is " + valid);
+        });
     }
 }
--- src/main/java/com/putoet/day4/PassPhrase.java
+++ src/main/java/com/putoet/day4/PassPhrase.java
@@ -1,19 +1,20 @@
 package com.putoet.day4;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class PassPhrase {
-    public static boolean isValidNoDoubles(String phrase) {
-        assert phrase != null;
-        final String[] words = phrase.split(" ");
+    public static boolean isValidNoDoubles(@NotNull String phrase) {
+        final var words = phrase.split(" ");
 
         if (words.length < 2)
             return false;
 
-        final Set<String> set = new HashSet<>();
-        for (String word : words) {
+        final var set = new HashSet<String>();
+        for (var word : words) {
             if (!word.matches("[a-z]+") || !set.add(word))
                 return false;
         }
@@ -21,16 +22,15 @@ public class PassPhrase {
         return true;
     }
 
-    public static boolean isValidNoAnagrams(String phrase) {
-        assert phrase != null;
-        final String[] words = phrase.split(" ");
+    public static boolean isValidNoAnagrams(@NotNull String phrase) {
+        final var words = phrase.split(" ");
 
         if (words.length < 2)
             return false;
 
-        final Set<String> set = new HashSet<>();
-        for (String word : words) {
-            char[] sorted = word.toCharArray(); Arrays.sort(sorted);
+        final var set = new HashSet<String>();
+        for (var word : words) {
+            final var sorted = word.toCharArray(); Arrays.sort(sorted);
             if (!word.matches("[a-z]+") || !set.add(String.valueOf(sorted)))
                 return false;
         }
